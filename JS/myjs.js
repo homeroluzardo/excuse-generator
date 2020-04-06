b25895e89b0928f7fd7b6fb23ef519ccbaeea8ab
Homero Luzardo <homeroeluzardo@gmail.com>
2020-04-05 21:49:23 -0400

    Excuse Generator

---
+++ JS/myjs.js
@@ -0,0 +1,19 @@
+		window.onload = () => {
+            document.querySelector("#excuse").innerHTML = generateExcuse();
+            document.querySelector("#btn").addEventListener("click", () =>{
+                document.querySelector("#excuse").innerHTML = generateExcuse();
+            });
+		}
+
+		let generateExcuse = () => {
+
+			let who = ['the dog','my granma','his turtle','my bird'];
+			let what = ['eat','pissed','crushed','broked'];
+			let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+			let whoIndex = Math.floor(Math.random() * who.length);
+			let whatIndex = Math.floor(Math.random() * what.length);
+			let whenIndex = Math.floor(Math.random() * when.length);
+
+			return who[whoIndex]+' '+what[whatIndex]+' '+when[whenIndex];
+		}
